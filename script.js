54653d9de6a9bd7043c5b56fcda0f7686fcd225a
Serg-28 <sergbondarenko28@gmail.com>
2021-04-22 20:50:27 +0300

    feat:add a video element with functions that control video

--- script.js
+++ script.js
@@ -21,3 +21,44 @@ function light(sw) {
 /*После проверки получаем элемент по id (img tag) и указываем какой должен быть путь к источнику фото(сохраненный в нашей переменной pic)  */      
     document.getElementById('myImage').src = pic;
 }
+
+/* Создаем функции для обработки видео из index.html  */
+
+// Сперва создадим переменную, которая с помощью document.getElementById найдет нам элмент с нужным айди
+// который мы будем обрабатывать ("video1")
+let myVideo = document.getElementById("video1");
+
+/* Функция, которая проверяет DOM свойство - paused, и в зависимости от результата воспроизводит через
+метод play() или приостанавливает через метод  pause() */
+function playPause() { 
+    if (myVideo.paused) 
+        myVideo.play(); 
+    else 
+        myVideo.pause(); 
+}
+
+// Затем создадим функцию, которая будет увеличивать размер элемента video
+function makeBig(){
+  myVideo.width = 720;
+  myVideo.height = 480;
+}
+
+// Затем создадим функцию, которая будет уменьшать размер элемента video
+function makeSmall(){
+    myVideo.width = 240;
+    myVideo.height = 128;
+  }
+//Создадим функцию, которая вернет нормальные размеры экрана 
+  function makeNormal(){
+    myVideo.width = 420;
+    myVideo.height = 240;
+  }
+  
+// И на последок  две функци, которые будут менять уровень громкости. Первая включает половину, вторая делает громкость на полную 
+
+function makeQuieter () {
+    myVideo.volume = 0.5;
+}
+function makeLouder () {
+    myVideo.volume = 1;
+}
